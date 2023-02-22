73943d746f0976f879d72bf22fb96c48d16208c1
Austine Charles <ndubuisi.charles@decagon.dev>
2023-02-22 20:38:27 +0100

    SuperBankProject
    DrPamPam helped me achieve this project though.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using SuperBank.Core.UserInterface;
+
+namespace SuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface.Run();
+        }
+    }
+}
